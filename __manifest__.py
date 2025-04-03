8c806d3570c72b41f749f43bd6d93f1fa3f9e49f
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-04-03 23:13:04 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Fleet GPS',
+    'version': '18.0.1.0.0',
+    "summary": """
+        This module extends the Fleet module allowing gps tracking.""",
+    'depends': ['fleet'],
+    'application': True,
+    'installable': True,
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+}
