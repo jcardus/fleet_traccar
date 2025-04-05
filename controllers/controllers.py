fa2c2d13bc0e66dc937f7d8ce2faebe446354306
jcardus <asklocation.net@gmail.com>
2025-04-05 22:38:14 +0100

    remove csrf fix form data

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -8,15 +8,13 @@ import requests
 
 class ApiProxy(http.Controller):
 
-    @http.route('/fleet_traccar/api/<path:_path>', type='http', auth='user')
+    @http.route('/fleet_traccar/api/<path:_path>', type='http', auth='user', csrf=False)
     def proxy_request(self, _path):
         base_url = request.env['ir.config_parameter'].sudo().get_param(
             'fleet_traccar.api_base_url') or 'http://gps.frotaweb.com/api'
 
         if request.httprequest.method in ['POST', 'PUT']:
-            if 'application/json' in request.httprequest.headers.get('Content-Type', ''):
-                body = request.jsonrequest
-            elif 'application/x-www-form-urlencoded' in request.httprequest.headers.get('Content-Type', ''):
+            if 'application/x-www-form-urlencoded' in request.httprequest.headers.get('Content-Type', ''):
                 body = dict(request.httprequest.form)
             else:
                 body = request.httprequest.data
