bc10929087a79276764323670525a61ada87f8e8
jcardus <asklocation.net@gmail.com>
2025-04-05 19:42:23 +0100

    fix form data

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -12,10 +12,21 @@ class ApiProxy(http.Controller):
     def proxy_request(self, _path):
         base_url = request.env['ir.config_parameter'].sudo().get_param(
             'fleet_traccar.api_base_url') or 'http://gps.frotaweb.com/api'
+
+        if request.httprequest.method in ['POST', 'PUT']:
+            if 'application/json' in request.httprequest.headers.get('Content-Type', ''):
+                body = request.jsonrequest
+            elif 'application/x-www-form-urlencoded' in request.httprequest.headers.get('Content-Type', ''):
+                body = dict(request.httprequest.form)
+            else:
+                body = request.httprequest.data
+        else:
+            body = None
+
         response = requests.request(
             method=request.httprequest.method,
             url=f"{base_url.rstrip('/')}/{_path}?{request.httprequest.query_string.decode('utf-8')}",
             headers=dict(request.httprequest.headers),
-            data=request.httprequest.data if request.httprequest.method in ['POST', 'PUT'] else None,
+            data=body,
         )
         return request.make_response(response.content, status=response.status_code)
