b17ec4890680be4b9a81fbf36059cb258c760d00
jcardus <asklocation.net@gmail.com>
2025-04-07 01:00:34 +0100

    18.0.1.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Frotaweb',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     "summary": """
         This module extends the Fleet module allowing gps tracking.""",
     'depends': ['fleet'],
